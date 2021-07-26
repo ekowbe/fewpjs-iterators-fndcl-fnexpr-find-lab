9bdf5d2145251c112dcdacc408b190be91a26bb2
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-07-26 16:52:05 +0000

    functionality done. debugging needed.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+function superbowlWin(objs) {
+    objs.find(function(tournament) {
+        // console.log(tournament)
+        // console.log(tournament.result)
+        // console.log(tournament.year)
+        if (tournament.result === "W") {
+            return tournament.year
+        }
+        else {
+            return undefined
+        }
+    })
+}
+
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
